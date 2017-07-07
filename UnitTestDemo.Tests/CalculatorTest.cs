2b77316aaeceac0919e3c4988ced86002a437bd1
rajesh.kumar <rajesh.kumar@KELLGGNCPU0356>
2017-07-07 11:37:09 +0530

    Add project files.

---
+++ UnitTestDemo.Tests/CalculatorTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestDemo.Tests
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestSumMethod()
+        {
+            Calculator Calculator = new Calculator();
+            int Sum = Calculator.Sum(10, 33);
+            Assert.AreEqual(43, Sum);
+        }
+    }
+}
---
+++ UnitTestDemo/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDemo
+{
+    public class Calculator
+    {
+        public int Sum(int FirstVal, int secondVal)
+        {
+            return FirstVal + secondVal;
+        }
+    }
+}
