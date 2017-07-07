97dccee0a2974af89a1197af33eb17f2cd4999b7
rajesh.kumar <rajesh.kumar@KELLGGNCPU0356>
2017-07-07 11:57:44 +0530

    2

--- UnitTestDemo.Tests/CalculatorTest.cs
+++ UnitTestDemo.Tests/CalculatorTest.cs
@@ -11,7 +11,7 @@ namespace UnitTestDemo.Tests
         {
             Calculator Calculator = new Calculator();
             int Sum = Calculator.Sum(10, 33);
-            Assert.AreEqual(43, Sum);
+            Assert.AreEqual(4, Sum);
         }
     }
 }
